0c55628cbf722fa65fe477f03e3daf0b5f34a955
Muzammil <muzamilbhundi@gmail.com>
2021-12-08 23:57:32 +0500

    First grid task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import {makeStyles} from '@material-ui/core/styles'
+import { Container } from '@material-ui/core';
+const useStyles = makeStyles({
+  container:{
+    backgroundColor:"#23B14D",
+    border: '5px solid #000',
+    padding:'5px 5px'
+  },
+  boxStyle:{
+    backgroundColor: '#FFC90D',
+    border: '5px solid #000',
+    height:'350px',
+    width:'250px',
+    
 
+  },
+  boxStyle2:{
+    backgroundColor: '#FFC90D',
+    border: '5px solid #000',
+    height:'300px',
+    width:'200px',
+    marginLeft:'20px'
+  }
+
+})
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container fluid className={classes.container}>
+    <Grid container spacing={2}>
+    <Grid item md={3} >
+      <Box  className={classes.boxStyle} ></Box>
+
+    </Grid>
+    <Grid item md={4}  >
+      <Box className={classes.boxStyle} mx={3}></Box>
+
+    </Grid>
+    <Grid item md={5} >
+      <Box  className={classes.boxStyle} ></Box>
+
+    </Grid>
+    <Grid item md={3} >
+      <Box  className={ classes.boxStyle} m={2}></Box>
+
+    </Grid>
+    <Grid item md={4} >
+      <Box className={classes.boxStyle} mx={3}></Box>
+
+    </Grid>
+    <Grid item md={4} >
+      <Box className={classes.boxStyle}></Box>
+
+    </Grid>
+    </Grid>
+    </Container>
+    </>
   );
 }
 
